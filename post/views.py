dfdd3c17757d87603be7d8b24ca62a3feb393e2c
VeronikaSotskova <verasos@mail.ru>
2019-12-04 16:07:49 +0300

    add

---
+++ post/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+
+# Create your views here.
+from post.forms import PostForm
+from post.models import Post
+
+
+def index(request):
+    form = PostForm()
+    posts = Post.objects.all()
+    return render(request, 'index.html', context={'posts': posts, 'form': form})
+
+
+def add_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            Post.objects.create(text=form.cleaned_data('text'))
+            return HttpResponseRedirect('/')
